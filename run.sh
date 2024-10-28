40442e874d8455ee969803eefbb0439a967c302d
Sarnog <90943320+Sarnog@users.noreply.github.com>
2024-10-28 19:03:08 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 TARGET_IP=${TARGET_IP:-"192.168.1.72"}
-TARGET_PORT=${TARGET_PORT:-"8095"}
+TARGET_PORT=${TARGET_PORT:-"80"}
 
-# Start a simple proxy using curl
+# Start a simple proxy using nc
 while true; do
-    # Use a simple HTTP server to listen for requests
+    # Listen for incoming requests
     nc -l -p 8080 | while read line; do
         # Forward the request to the Music Assistant
         curl -s -X "${line}" "http://${TARGET_IP}:${TARGET_PORT}" &
